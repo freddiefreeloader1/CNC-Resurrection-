c527c681cb4ccea9b1b0d2d1364e4f8cd22ab6c6
freddiefreeloader1 <69372260+freddiefreeloader1@users.noreply.github.com>
2023-06-18 13:17:16 +0300

    Update ky040.py

--- Codes/Pico_W/ky040.py
+++ Codes/Pico_W/ky040.py
@@ -1,7 +1,7 @@
 import time
 from rotary_irq_rp2 import RotaryIRQ
 from lcd_trial import write_lcd as lcd
-from machine import Pin, PWM, ADC,  SPI
+from machine import Pin, PWM, ADC,  SPI, Timer
 import gc9a01 as gc9a01
 import vga1_16x16 as font
 import NotoSansMono_32 as font2
@@ -12,12 +12,13 @@ import socket
 ################################################# CONNECTION
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-wlan.connect("Name of the network" ,"password")
+# wlan.connect("SUPERONLINE_WiFi_3659","vujnDFh4PbeH")
+wlan.connect("POCO X3 Pro" ,"123456789")
 time.sleep(5)
 print(wlan.isconnected())
 # # # # # # # # ############################## opening socket
-SERVER_IP = 'Global IP adress'
-PORT = ****  
+SERVER_IP = '192.168.113.61'
+PORT = 5151   
 # Create a socket object
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -58,8 +59,37 @@ def callback_R(ButtonR):
 flag_R = ButtonR.value()
 
 ButtonR.irq(trigger = Pin.IRQ_RISING, handler = callback_R)
-# # # # # # # #
+##############################################################3
+ButtonY = Pin(1,Pin.IN,Pin.PULL_DOWN)
 
+
+global flag_Y
+global counterY
+global axisJ
+
+axisJ = ["|X| ","|Y| ","|XY|"]
+counterY = 0
+def callback_Y(ButtonY):
+    global flag_Y
+    global counterY
+    ButtonY.irq(handler = None)
+
+    time.sleep(0.01)
+    if flag_Y is 0 and ButtonY.value() == 1:
+        # print("Interrupt has occured")
+        flag_Y = 0
+        counterY = counterY + 1
+        print(counterY)
+        if counterY == 3:
+            counterY = 0
+
+
+    flag_Y = ButtonY.value()
+    ButtonY.irq(handler = callback_Y)
+
+flag_Y = ButtonY.value()
+
+ButtonY.irq(trigger = Pin.IRQ_RISING, handler = callback_Y)
 ############################################################################################## joystick
 vrx = ADC(Pin(26))
 vry = ADC(Pin(27))
@@ -77,7 +107,16 @@ def create_array(xval,yval):
         ynum = -1
     return [xnum,ynum]
 #######################################################################################################
+###############################
+def interruption_handler(timer):
+    client_socket.send("HB".encode())
 
+
+soft_timer = Timer(mode=Timer.PERIODIC, period=1000, callback=interruption_handler)
+################################Buzzer
+buzz = PWM(Pin(4))
+buzz.freq(1000)
+###############################
 spi = SPI(0, baudrate=60000000, sck=Pin(6), mosi=Pin(3))
 tft = gc9a01.GC9A01(
     spi,
@@ -87,41 +126,52 @@ tft = gc9a01.GC9A01(
     rotation=0)
 
 
-
-
 r = RotaryIRQ(pin_num_clk=13,
               pin_num_dt=12,
               min_val=0,
               max_val=36,
               reverse=False,
               range_mode=RotaryIRQ.RANGE_WRAP)
-data = {"joystick":"", "encoder":"","axis":""}
+
+data = {"joystick":"", "encoder":"","axis":"","axisJ":""}
 val_old = r.value()
 tft.fill(0)
 old_val_new = 0
 data_k = ""
-old_data = {}
+old_data = {"joystick":"", "encoder":"","axis":"","axisJ":""}
 while True:
+    
     xval = vrx.read_u16()
     yval = vry.read_u16()
     data_j = str(create_array(xval,yval))
     val_new = r.value()
-    lcd(tft,axis[counter],110,70,10000)
+    lcd(tft,axisJ[counterY],105,70,10000)
+    lcd(tft,axis[counter],80,70,10000)   
     if val_old != val_new:
-        lcd(tft,".",int(80*cos(old_val_new/5.75))+110,int(80*sin(old_val_new/5.75)+110),0)
+        buzz.duty_u16(50000)
+        lcd(tft,".",int(90*cos(old_val_new/5.75))+110,int(90*sin(old_val_new/5.75)+110),0)
 
         tft.fill_rect(100, 110, 70,30, 0)
         val_old = val_new
         # print('result =', val_new)
         data_k = str(val_new*10)
         lcd(tft,data_k,100,110,30000)
-        lcd(tft,".",int(80*cos(val_new/5.75))+110,int(80*sin(val_new/5.75)+110),4000)
+        lcd(tft,".",int(90*cos(val_new/5.75))+110,int(90*sin(val_new/5.75)+110),4000)
         old_val_new = val_new
+    buzz.duty_u16(0)
     data["joystick"] = data_j
-    data["encoder"] = data_k
+    data["encoder"] = data_k + "|"
     data["axis"] = axis[counter]
-    if data != old_data:
-        client_socket.send(str(data).encode())
+    data["axisJ"] = axisJ[counterY]
+    if data["joystick"] != old_data["joystick"]:
+        client_socket.send(str(data["joystick"]).encode())
+    if data["encoder"] != old_data["encoder"]:
+        client_socket.send(str(data["encoder"]).encode())
+    if data["axis"] != old_data["axis"]:
+        client_socket.send(str(data["axis"]).encode())
+    if data["axisJ"] != old_data["axisJ"]:
+        client_socket.send(str(data["axisJ"]).encode())
     old_data = data.copy()
 
-    time.sleep_ms(10)
+    time.sleep_ms(1)
+    
